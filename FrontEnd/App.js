631fe1907975f76cf7189b03527bc5fca84fb06b
deniii1753 <deniii1753@gmail.com>
2023-08-03 20:00:43 +0300

    Add Menu in main page

--- FrontEnd/App.js
+++ FrontEnd/App.js
@@ -1,10 +1,31 @@
-import { StyleSheet, View } from 'react-native';
+import { Image, Dimensions, StyleSheet, Text, View, Pressable } from 'react-native';
 import Logo from './Components/Logo';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Logo />
+
+      {/* <Menu /> */}
+
+      <View style={styles.listContainer}>
+        <Pressable style={styles.containerWrapper}>
+          <Image source={require('./assets/new-order.png')} style={styles.icon}/>
+          <Text>Нова Поръчка</Text>
+        </Pressable>
+        <Pressable style={styles.containerWrapper}>
+          <Image source={require('./assets/clients.png')} style={styles.icon}/>
+          <Text>Клиенти</Text>
+        </Pressable>
+        <Pressable style={styles.containerWrapper}>
+          <Image source={require('./assets/all-orders.png')} style={styles.icon}/>
+          <Text>История на поръчки</Text>
+        </Pressable>
+        <Pressable style={styles.containerWrapper}>
+          <Image source={require('./assets/stock.png')} style={styles.icon}/>
+          <Text>Продукти</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -16,5 +37,27 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     paddingHorizontal: 20,
     paddingVertical: 5
+  },
+
+  listContainer: {
+    flex: 1,
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    marginTop: 20,
+    gap: 10
+  },
+
+  icon: {
+    marginBottom: 10
+  },
+
+  containerWrapper: {
+    width: (Math.round(Dimensions.get('window').width / 2)) - 30,
+    height: "25%",
+    backgroundColor: "#E86161",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 5
   }
 });
