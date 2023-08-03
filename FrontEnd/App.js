64f7ce62e4de53489e9856adee0cfcb34086abd7
deniii1753 <deniii1753@gmail.com>
2023-08-03 18:42:19 +0300

    Add logo component

--- FrontEnd/App.js
+++ FrontEnd/App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Logo from './Components/Logo';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Logo />
     </View>
   );
 }
@@ -13,8 +12,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: '#202020',
+    justifyContent: 'flex-start',
+    paddingHorizontal: 20,
+    paddingVertical: 5
+  }
 });
---
+++ FrontEnd/Components/Logo.js
@@ -0,0 +1,23 @@
+import { View, Image, StyleSheet } from 'react-native';
+
+export default function Logo() {
+    return (
+        <View style={styles.logoContainer}>
+            <Image source={require('../assets/logo.png')} style={styles.logo} />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    logoContainer: {
+        width: '100%',
+        height: 150,
+        overflow: 'hidden',
+    },
+
+    logo: {
+        objectFit: 'contain',
+        width: '100%',
+        height: '100%'
+    }
+});
