2860d3d1a81d5c225cbe939653f8571ff2f6a2a4
deniii1753 <deniii1753@gmail.com>
2023-08-04 11:28:23 +0300

    Fixed screen flashing while navigating

--- FrontEnd/App.js
+++ FrontEnd/App.js
@@ -1,26 +1,30 @@
 import { StyleSheet, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as SystemUI from 'expo-system-ui';
 
 import HomeScreen from './Components/HomeScreen/HomeScreen';
+import NewOrderScreen from './Components/NewOrderScreen/NewOrderScreen';
+
+SystemUI.setBackgroundColorAsync('#202020')
+  .catch(err => console.log(`error: ${err.message}`));
 
 export default function App() {
-
   const Stack = createNativeStackNavigator();
 
   return (
     <NavigationContainer>
       <Stack.Navigator
         initialRouteName='Home'
-        screenOptions={{ 
+        screenOptions={{
           headerShown: false,
           contentStyle: styles.container
         }}
       >
-        <Stack.Screen
-          name='Home'
-          component={HomeScreen}
-        />
+
+        <Stack.Screen name='Home' component={HomeScreen} />
+        <Stack.Screen name='NewOrder' component={NewOrderScreen} />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
