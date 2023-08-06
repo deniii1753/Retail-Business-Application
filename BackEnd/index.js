432116390eab3fed33ebdcb404219248f8763f8f
deniii1753 <deniii1753@gmail.com>
2023-08-06 13:31:40 +0300

    Added environmental variables
    Also refactored the index.js on the backend and added cors to the app

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const mongoose = require('mongoose');
-
-const PORT = 3030;
+require('dotenv').config();
+const { connectDatabase } = require('./config/database.js');
+const PORT = process.env.PORT || 3030;
 
 const app = express();
 
-mongoose.connect('mongodb://127.0.01:27017/RetailBusiness', )
+connectDatabase()
     .then(() => app.listen(PORT, () => console.log(`Database connected successfully! Server listening on port ${PORT}!`)))
-    .catch(err => console.error(`AN error occured!: ${err.message}`));
+    .catch(err => console.error(`An error occured!: ${err.message}`));
